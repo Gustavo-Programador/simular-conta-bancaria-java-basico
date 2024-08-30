2661cc7c57032f17e3418158d539e733990322e6
Gustavo <gustavodejesusfernandes48@gmail.com>
2024-08-30 16:08:46 -0300

    Projeto basico feito

--- SimularContaBancaria/src/ContaTerminal.java
+++ SimularContaBancaria/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in). useLocale(Locale.US);
+
+        System.out.println("Digite o número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da conta: ");
+        int numero = scanner.nextInt();
+
+        String nomeCliente = "Gustavo de Jesus";
+
+        double saldo = 250;
+
+        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "  + agencia +", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque" );
+
+        
+
+
+        
+        
+    
     }
 }
